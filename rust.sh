febf1967ca142305745a65ed36506598dcf59db9
Ravi Gupta <ravgupta20@gmail.com>
2026-05-24 18:40:36 -0400

    added rush script

---
+++ rust.sh
@@ -0,0 +1,6 @@
+# ==========================================
+# RUST SHORTCUTS
+# ==========================================
+alias rust-start="cargo new" # Follow with project name: rust-start my-app
+alias rust-run="cargo run"
+alias rust-test="cargo test"
