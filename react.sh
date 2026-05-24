a7ca83270e331d659234f28815932965db07369d
Ravi Gupta <ravgupta20@gmail.com>
2026-05-24 18:43:03 -0400

    added react script

---
+++ react.sh
@@ -0,0 +1,8 @@
+bash
+# ==========================================
+# JAVASCRIPT / REACT SHORTCUTS (Vite + TS)
+# ==========================================
+
+# Creates a brand new React + TypeScript project instantly using Vite
+# Usage: js-start-react my-app-name
+alias react="npm create vite@latest -- --template react-ts"
